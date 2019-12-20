f030538888fd097d6ed7a592acbe3840d7bb78e4
Deep <42325107+Deep4569@users.noreply.github.com>
2019-12-19 19:58:19 -0500

    Updated Loading Page

---
+++ gatsby-config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+
+module.exports = {
+  plugins: [
+    {
+      `gatsby-plugin-react-helmet`,
+      `gatsby-plugin-styled-components`,
+      `gatsby-plugin-sharp`,
+      `gatsby-transformer-sharp`,
+      `gatsby-plugin-sitemap`,
+      `gatsby-plugin-robots-txt`,
+      {
+        resolve: `gatsby-plugin-manifest`,
+        options: {
+          name: 'DeepPatel',
+          short_name: 'DeepPatel',
+          start_url: '/',
+          background_color: '#020c1b',
+          theme_color: '#0a192f',
+          display: 'minimal-ui',
+          icon: 'src/images/logo.png',
+        },
+    },
+    {
+      resolve: `gatsby-plugin-alias-imports`,
+      options: {
+        alias: {
+          '@components': path.resolve(__dirname, 'src/components'),
+          '@config': path.resolve(__dirname, 'src/config'),
+          '@fonts': path.resolve(__dirname, 'src/fonts'),
+          '@images': path.resolve(__dirname, 'src/images'),
+          '@pages': path.resolve(__dirname, 'src/pages'),
+          '@styles': path.resolve(__dirname, 'src/styles'),
+          '@utils': path.resolve(__dirname, 'src/utils'),
+        },
+        extensions: [
+          "js",
+        ],
+      }
+    }
+    // your another plugins
+  ]
+};
